1fad547df374912588c1dbd324fd020dc60db79f
STEVE LEONEL YOMI MBIAKOP <leonelyomi3@gmail.com>
2024-12-05 14:23:22 +0100

    add springboot-file

---
+++ springboot/src/test/java/com/leonel/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonel.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
